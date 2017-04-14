fbda73d1fcca7e7a3db554e1aeb7d56efd81f481
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-13 17:09:06 -0700

    Hierarchies - Day 1 Code Review 1

--- melons.py
+++ melons.py
@@ -1,9 +1,13 @@
-"""Classes for melon orders."""
+"""
+
+Classes for melon orders.
+
+"""
 
 
 # Establish Super Class AbstractMelonOrder
 class AbstractMelonOrder(object):
-    """A melon order from ______."""
+    """ A melon order from ______."""
 
     def __init__(self, species, qty):
         """Initialize melon order attributes."""
@@ -11,8 +15,8 @@ class AbstractMelonOrder(object):
         self.species = species
         self.qty = qty
         self.shipped = False
- #       self.order_type = "abstract"
         self.tax = 0
+        # self.order_type = "abstract"
 
     def get_total(self):
         """Calculate price, including tax."""
@@ -27,6 +31,7 @@ class AbstractMelonOrder(object):
 
         self.shipped = True
 
+
 # Establish sub-class DomesticMelonOrder
 class DomesticMelonOrder(AbstractMelonOrder):
     """A melon order within the USA."""
@@ -35,25 +40,9 @@ class DomesticMelonOrder(AbstractMelonOrder):
     def __init__(self, order_type):
         super(DomesticMelonOrder, self).__init__(order_type, "domestic")
 
+        self.tax = 0.08
+
     """Initialize melon order attributes."""
-
-    # self.species = species
-    # self.qty = qty
-    # self.shipped = False
-    self.tax = 0.08
-
-    # def get_total(self):
-    #     """Calculate price, including tax."""
-
-    #     base_price = 5
-    #     total = (1 + self.tax) * self.qty * base_price
-
-    #     return total
-
-    # def mark_shipped(self):
-    #     """Record the fact than an order has been shipped."""
-
-    #     self.shipped = True
 
 
 # Establish sub-class InternationalMelonOrder
@@ -64,30 +53,14 @@ class InternationalMelonOrder(AbstractMelonOrder):
     def __init__(self, order_type):
         super(InternationalMelonOrder, self).__init__(order_type, "international")
 
-    """Initialize melon order attributes."""
-
-        # self.species = species
-        # self.qty = qty
         self.country_code = country_code
-        # self.shipped = False
         self.tax = 0.17
 
-    # def get_total(self):
-    #     """Calculate price, including tax."""
-
-    #     base_price = 5
-    #     total = (1 + self.tax) * self.qty * base_price
-
-    #     return total
-
-    # def mark_shipped(self):
-    #     """Record the fact than an order has been shipped."""
-
-    #     self.shipped = True
+    """Initialize melon order attributes."""
 
     def get_country_code(self):
         """Return the country code."""
 
         return self.country_code
 
-# 4:30p Day 1 - Waiting for Code Review
+# Code Review by Ahamd # ZOMG AWESOME!!!!!!
